f3b1d4dbf3f0e291e540704d11aad36d26aab478
ajaykumbhar01 <118867010+ajaykumbhar01@users.noreply.github.com>
2022-12-09 22:27:44 +0530

    swap assignment
    swap assignment

---
+++ 01_swap_variable.js
@@ -0,0 +1,24 @@
+console.log("---Using Three Number---");
+console.log("Before Swap" );
+var num1=100;
+var num2=200;
+var num3=300;
+console.log(num1,num2,num3);
+console.log("After Swap");
+var temp=100;
+
+num1=num2
+num2=num3
+num3=temp
+console.log(num1,num2,num3);
+console.log("===Using Two Number===");
+console.log("---Before Swap---");
+var sweety="Sweety"
+var cutie="Cutie"
+console.log(sweety,cutie);
+
+var niti= "Sweety"
+console.log("---after Swap---");
+sweety=cutie
+cutie=niti
+console.log(sweety,cutie);
