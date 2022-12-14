9b3a79f90c503a165e4a67b165782005b4eb000a
ajaykumbhar01 <118867010+ajaykumbhar01@users.noreply.github.com>
2022-12-14 22:24:58 +0530

    string assignment
    string assignment

---
+++ 04_arithmaticOperator.js
@@ -0,0 +1,33 @@
+var num1 = 10;
+var num2 = 5;
+
+console.log(`============ Arithmetic Operators =================`);
+var addResult = num1 + num2;
+console.log(`Addition is ${addResult} `);
+
+console.log(`Subtraction is ${num1-num2}`);
+var mulResult  = num1 * num2;
+console.log(`Multiplication is ${mulResult}`);
+ 
+var divResult = num1 / num2;
+console.log(`Division is ${divResult}`);
+
+var modulusResult = num1 % num2;
+console.log(`Modulus is ${modulusResult}`);
+
+var modulusResult = num1 % 4;
+console.log(`Modulus is ${modulusResult}`);
+
+console.log(`============ Assignment Operators =================`);
+var num1=10
+var num2=5
+num1+=num2
+console.log(`add is ${num1+=num2}`);
+console.log(`============ Unary Operators =================`);
+var num3 = 10;
+var result = ++num3; // num3 = num3 + 1;
+console.log(`Increment operator  result is ${result}`);
+
+var num4 = 5;
+var res = --num4; //num4 -1;
+console.log(`Decrement operator result is ${res}`);
---
+++ stringAssignment_01.js
@@ -0,0 +1,18 @@
+var value = "squareWordLenghth";
+console.log("Length of string:",value.length);
+console.log("square of lengt:",value.length * value.length);
+console.log("===function for values===");
+var wordLenghthSquare = function (strArg) {
+  console.log("JavaScript");
+  console.log("Google Chrome");
+  console.log("Develper Smart");
+};
+wordLenghthSquare();
+console.log("=================================");
+
+var sentence ="i am react developer"
+var wordinsentence= sentence.split(" ")
+console.log("Totalno of word available in string:",wordinsentence.length);
+var sentence ="i am react developer"
+var wordinsentence= sentence.split(" ")
+console.log("Multiple by the total word:",wordinsentence.length*wordinsentence.length);
